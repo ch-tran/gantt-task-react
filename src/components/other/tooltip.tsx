659fa6309eb336c4095e88524ad9fca73f531f15
ctran634 <Chau_Tran@comcast.com>
2022-08-18 12:40:49 -0400

    Tooltip scaled to minutes

--- src/components/other/tooltip.tsx
+++ src/components/other/tooltip.tsx
@@ -125,21 +125,17 @@ export const StandardTooltipContent: React.FC<{
     <div className={styles.tooltipDefaultContainer} style={style}>
       <b style={{ fontSize: fontSize + 6 }}>{`${
         task.name
-      }: ${task.start.getDate()}-${
-        task.start.getMonth() + 1
-      }-${task.start.getFullYear()} - ${task.end.getDate()}-${
-        task.end.getMonth() + 1
-      }-${task.end.getFullYear()}`}</b>
+      }: ${task.start.getHours()}:${
+        task.start.getMinutes() + 1
+      }:${task.start.getSeconds()} - ${task.end.getHours()}:${
+        task.end.getMinutes() + 1
+      }:${task.end.getSeconds()}`}</b>
       {task.end.getTime() - task.start.getTime() !== 0 && (
         <p className={styles.tooltipDefaultContainerParagraph}>{`Duration: ${Math.abs(
           (task.end.getTime() - task.start.getTime()) /
           (1000 * 60) % 60
         )} minute(s)`}</p>
       )}
-
-      <p className={styles.tooltipDefaultContainerParagraph}>
-        {!!task.progress && `Progress: ${task.progress} %`}
-      </p>
     </div>
   );
 };
